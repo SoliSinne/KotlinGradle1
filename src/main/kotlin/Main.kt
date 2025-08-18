72906edc8749d0af8c2bd7d998e54e26b2629160
SoliSinne <joakim.sohlberg.swe@gmail.com>
2025-08-18 14:20:10 +0200

    chi-square-lib

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,32 +7,15 @@ import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kotlinx.serialization.json.Json
-import org.example.api.Decision
 import org.example.api.InputData
-import kotlin.time.Clock
-import kotlin.time.ExperimentalTime
 
-@OptIn(ExperimentalTime::class)
 fun main() {
     embeddedServer(Netty, 8080) {
         routing {
-            get("/abc") {
-                call.respondText("Hello World!")
-            }
-            get("/decision") {
-                println("WHAT?")
-                val decision = Decision(1, 0, decision = "NOT")
-                call.respondText(decision.toString(), ContentType.Text.Plain, HttpStatusCode.OK)
-            }
-            post("/decide") {
-                val decision = Json.decodeFromString<Decision>(call.receiveText())
-                println("Decision: $decision, ${Clock.System.now()}")
-                call.respondText(decision.toString(), ContentType.Text.Plain, HttpStatusCode.Created)
-            }
             post("/check") {
                 val inputData = Json.decodeFromString<InputData>(call.receiveText())
-                println("InputData: $inputData, ${Clock.System.now()}")
-                call.respondText("OK", ContentType.Text.Plain, HttpStatusCode.Created)
+                val decision = theChiSquareTestLib(inputData)
+                call.respondText(decision.toString(), ContentType.Text.Plain, HttpStatusCode.Created)
             }
         }
     }.start(wait = true)
